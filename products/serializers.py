b3a872c6198784da2a679667a4ac4cde3dc329cb
esmaeel116 <esmaeelzohari1382@gmail.com>
2025-05-05 15:17:34 +0330

    feat: update product-related files (admin, serializers, urls, views)

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug']
